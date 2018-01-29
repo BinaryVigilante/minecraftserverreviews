044d9a35a7a616b2acd5bea40f7446ed85523f77
BinaryVigilante <mail@binaryvigilante.com>
2018-01-29 01:05:28 +0100

    Fix title

--- inc/menu.php
+++
@@ -1,7 +0,0 @@
-<div class="leftColumn">
-    <h2 class="menu-header-text">Menu:</h2>
-        <p><a href="/index.html" class="menu-button"><b>Home</b></a></p>
-	      <p><a href="/reviews.html" class="menu-button"><b>Reviews</b></a></p>
-	      <p><a href="/contact.html" class="menu-button"><b>Contact</b></a></p>
-        <p><a href="/credits.html" class="menu-button"><b>Credits</b></a></p>
-</div>
